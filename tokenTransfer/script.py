4e8f59c76752f83444b9df5df718a5f42bb93169
tyGavinZJU <tianyaogavin@gmail.com>
2020-03-05 15:23:31 +0800

    de2

--- tokenTransfer/script.py
+++ tokenTransfer/script.py
@@ -1,7 +1,7 @@
 import subprocess
 
 f = open('redpocket.txt')
-
+f2 = open('log.txt', 'w')
 count = 0
 data = []
 def isSpecial(i):
@@ -23,7 +23,7 @@ while True:
     count = count + 1
 
 #print(data)
-
+count = 0
 for item in data:
     #print(item)
     amount = item[0]
@@ -31,6 +31,16 @@ for item in data:
     memo = ""
     if len(item) == 3:
         memo =  item[2]
-    print(amount,address,memo)
+    #print(amount,address,memo)
     cmd = "blockstack-cli balance " +address
-    subprocess.call(cmd, shell=True)
+    t =  subprocess.call(cmd, shell=True)
+    if t == 1 :
+        print(count,item)
+    f2.write(str(count)+":"+str(item)+":"+str(t)+"\n")
+    count = count + 1
+    #print("t is:",t)
+
+#t = subprocess.call("blockstack-cli balance 1234",shell=True)
+#print("t is:",t)
+f.close()
+f2.close()
